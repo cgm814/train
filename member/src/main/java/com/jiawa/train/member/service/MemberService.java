e266b3bc5924851a976f22b2b10295491efeaf34
chenguiming <guimingchen814@gmail.com>
2025-05-17 13:52:33 +0800

    4.20 使用雪花算法生成数据库id

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.jiawa.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.IdUtil;
 import com.jiawa.train.common.exception.BusinessException;
 import com.jiawa.train.common.exception.BusinessExceptionEnum;
+import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
@@ -27,19 +29,19 @@ public class MemberService {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
 
-    public long register(MemberRegisterReq req){
+    public long register(MemberRegisterReq req) {
         String mobile = req.getMobile();
         MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> list = memberMapper.selectByExample(memberExample);
 
-        if (CollUtil.isNotEmpty(list)){
+        if (CollUtil.isNotEmpty(list)) {
             // return list.get(0).getId();
             throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
 
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());  // 雪花算法
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
