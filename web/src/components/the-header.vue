af5f66d57d054ba3f8917a22a0d6bc5125b975d6
chenguiming <guimingchen814@gmail.com>
2025-05-19 01:19:42 +0800

    6.6 vuex配合h5 session缓存，解决浏览器刷新丢失数据的问题

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,12 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
+    <div style="float: right; color: white;">
+      您好：{{ member.mobile }} &nbsp;&nbsp;
+      <router-link to="/login" style="color: white;">
+        退出登录
+      </router-link>
+    </div>
     <a-menu v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
       <a-menu-item key="1">nav 1</a-menu-item>
       <a-menu-item key="2">nav 2</a-menu-item>
