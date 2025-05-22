942a2106b3e0193a8cca56a11e9970fa2dd8c74a
chenguiming <guimingchen814@gmail.com>
2025-05-22 22:31:54 +0800

    7.5 给SpringMvcConfig类增加@Configuration使其配置生效

--- member/src/main/java/com/jiawa/train/member/config/SpringMvcConfig.java
+++ member/src/main/java/com/jiawa/train/member/config/SpringMvcConfig.java
@@ -3,6 +3,7 @@ package com.jiawa.train.member.config;
 import com.jiawa.train.common.interceptor.LogInterceptor;
 import com.jiawa.train.common.interceptor.MemberInterceptor;
 import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Date: 2025/5/22 21:56
  * Description: SpringMvcConfig
  */
+@Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
     @Resource
