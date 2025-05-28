67375ed704493656caec8ffed4ae90673ec09b05
chenguiming <guimingchen814@gmail.com>
2025-05-28 22:57:38 +0800

    9.3 admin去掉登录页面，去掉member和登录相关的拦截，去掉乘车人页面

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,70 +1,77 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import store from "@/store";
-import { notification } from "ant-design-vue";
+import {createRouter, createWebHistory} from 'vue-router'
 
-const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // },
-  {
-    path: '/login',
-    component: () => import('../views/login.vue')
-  },
-  {
+const routes = [{
     path: '/',
     component: () => import('../views/main.vue'),
-    meta: {
-      loginRequire: true
-    },
-    children: [
-      {
+    children: [{
         path: 'welcome',
         component: () => import('../views/main/welcome.vue'),
-      },
-      {
-        path: 'passenger',
-        component: () => import('../views/main/passenger.vue'),
-      }
-    ]
-  },
-  {
+    }, {
+        path: 'about',
+        component: () => import('../views/main/about.vue'),
+    }, {
+        path: 'base/',
+        children: [{
+            path: 'station',
+            component: () => import('../views/main/base/station.vue'),
+        }, {
+            path: 'train',
+            component: () => import('../views/main/base/train.vue'),
+        }, {
+            path: 'train-station',
+            component: () => import('../views/main/base/train-station.vue'),
+        }, {
+            path: 'train-carriage',
+            component: () => import('../views/main/base/train-carriage.vue'),
+        }, {
+            path: 'train-seat',
+            component: () => import('../views/main/base/train-seat.vue'),
+        }]
+    }, {
+        path: 'business/',
+        children: [{
+            path: 'sk-token',
+            component: () => import('../views/main/business/sk-token.vue'),
+        }, {
+            path: 'confirm-order',
+            component: () => import('../views/main/business/confirm-order.vue'),
+        }, {
+            path: 'daily-train',
+            component: () => import('../views/main/business/daily-train.vue'),
+        }, {
+            path: 'daily-train-station',
+            component: () => import('../views/main/business/daily-train-station.vue'),
+        }, {
+            path: 'daily-train-carriage',
+            component: () => import('../views/main/business/daily-train-carriage.vue'),
+        }, {
+            path: 'daily-train-seat',
+            component: () => import('../views/main/business/daily-train-seat.vue'),
+        }, {
+            path: 'daily-train-ticket',
+            component: () => import('../views/main/business/daily-train-ticket.vue'),
+        }]
+    }, {
+        path: 'batch/',
+        children: [{
+            path: 'job',
+            component: () => import('../views/main/batch/job.vue')
+        }]
+    }, {
+        path: 'member/',
+        children: [{
+            path: 'ticket',
+            component: () => import('../views/main/member/ticket.vue')
+        }]
+    }]
+}, {
     path: '',
     redirect: '/welcome'
-  }
-];
+}];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
-
-// 路由登录拦截
-router.beforeEach((to, from, next) => {
-  // 要不要对meta.loginRequire属性做监控拦截
-  if (to.matched.some(function (item) {
-    console.log("是否需要登录校验：", item.meta.loginRequire || false);
-    return item.meta.loginRequire
-  })) {
-    const _member = store.state.member;
-    console.log("页面登录校验开始：", _member);
-    console.log("token: ",_member.token);
-    if (!_member.token) {
-      console.log("用户未登录或登录超时！");
-      notification.error({ description: "未登录或登录超时" });
-      next('/login');
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
-});
 
 export default router
--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -1,25 +1,9 @@
-import { createStore } from 'vuex';
-
-const MEMBER = "MEMBER";
+import {createStore} from 'vuex'
 
 export default createStore({
-  state: {
-    // member: window.sessionStorage.getItem(MEMBER) || {}
-    // 从sessionStorage读取数据（需解析JSON字符串，若不存在则初始化为空对象）
-    member: JSON.parse(window.sessionStorage.getItem(MEMBER)) || {}
-  },
-  getters: {
-  },
-  mutations: {
-    setMember(state, _member) {
-      state.member = _member;
-      // window.sessionStorage.setItem(MEMBER, _member);
-      // 保存到sessionStorage（需将对象序列化为JSON字符串）
-      window.sessionStorage.setItem(MEMBER, JSON.stringify(_member));
-    }
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {}
 })
